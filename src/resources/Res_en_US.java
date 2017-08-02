d6c9cba56ebe7c31fd304566812f303f26cffd81
Bertoncelli Giovanni <bertonc96@gmail.com>
2017-08-02 18:56:22 +0200

    Create Res_en_US.java

---
+++ src/resources/Res_en_US.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Res_en_US extends ListResourceBundle {
+
+  private static final Object[][] contents = {
+		{ "m_file", "File" },
+    { "m_open", "Apri" } 
+   };
+ 
+ @Override
+ public Object[][] getContents() {
+  return contents;
+ }
+}
