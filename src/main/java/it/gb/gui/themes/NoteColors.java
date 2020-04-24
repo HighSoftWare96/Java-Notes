e086dc7a1cb1fb17a5745bd4140c26569d8d1aeb
barbeque-squared <barbeque.squared@gmail.com>
2020-04-24 12:07:08 +0200

    rename themes

--- src/main/java/it/gb/gui/themes/NoteColors.java
+++ src/main/java/it/gb/gui/themes/NoteColors.java
@@ -4,25 +4,26 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 public class NoteColors {
-	// color[0] = mainColor // color[1] = backColor // color[2] = secColor
 	private static ArrayList<ColorComponent> colors = new ArrayList<>();
 	private static NoteColors instance = null;
 
+	// TODO: remove this suppression once the theme names are either internationalized, or we don't need to care about it anymore
+	@SuppressWarnings("SpellCheckingInspection")
 	private NoteColors() {
-		// TEMA VIOLA
+		// VIOLA
 		colors.add(new ColorComponent(Color.decode("#d98cb3"), Color.decode("#993366"), Color.decode("#cc6699"),
-				"tema viola"));
-		// TEMA VERDE
+				"purple theme"));
+		// VERDE
 		colors.add(new ColorComponent(Color.decode("#5cd65c"), Color.decode("#009933"), Color.decode("#88cc00"),
-				"tema verde"));
-		// TEMA BLU (DEFAULT)
+				"green theme"));
+		// BLU (DEFAULT)
 		colors.add(new ColorComponent(Color.decode("#66ccff"), Color.decode("#3399ff"), Color.decode("#99ccff"),
-				"tema blue"));
-		// TEMA ARANCIO
+				"blue theme"));
+		// ARANCIO
 		colors.add(new ColorComponent(Color.decode("#ffa64d"), Color.decode("#e67300"), Color.decode("#ffa366"),
-				"tema arancio"));
-		// TEMA BIANCO
-		colors.add(new ColorComponent(Color.WHITE, Color.LIGHT_GRAY, Color.WHITE, "tema bianco"));
+				"orange theme"));
+		// BIANCO
+		colors.add(new ColorComponent(Color.WHITE, Color.LIGHT_GRAY, Color.WHITE, "white theme"));
 	}
 
 	public static NoteColors getInstance() {
